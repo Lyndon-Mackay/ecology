b1ae38d91b0bb4df8f43751349a19543809cac4f
lyndon <lyndondmackay@gmail.com>
2020-03-15 14:54:07 +1100

    Add moevment for lumberjacks

--- src/main.rs
+++ src/main.rs
@@ -150,6 +150,17 @@ impl Forest {
             })
             .collect()
     }
+    fn get_lumberjacks(&self) -> Vec<(usize, usize, Woodcutter)> {
+        self.layout
+            .iter()
+            .enumerate()
+            .filter_map(|(i, x)| match *x {
+                ForestFeature::LumberJack(l) => Some((i / self.size, i % self.size, l)),
+                ForestFeature::LumberSeedling(l, _) => Some((i / self.size, i % self.size, l)),
+                _ => None,
+            })
+            .collect()
+    }
 
     fn age_trees(&mut self) {
         self.layout = self.layout.iter().map(|x| x.age()).collect();
@@ -166,17 +177,22 @@ impl Forest {
     }
     fn get_potential_spawn_sites(&self, x: usize, y: usize) -> Vec<(usize, usize)> {
         let mut returned_vec = Vec::with_capacity(8);
-        for delta_x in [self.size as u32 - 1, 0, 1].iter().cloned() {
-            for delta_y in [self.size as u32 - 1, 0, 1].iter().cloned() {
-                let new_x = delta_x as usize + x;
-                let new_y = delta_y as usize + y;
+        for delta_x in [-1, 0, 1].iter().cloned() {
+            for delta_y in [-1, 0, 1].iter().cloned() {
+                let new_x = delta_x + x as i32;
+                let new_y = delta_y + y as i32;
 
-                if (x as u32 + delta_x == 0 && y as u32 + delta_y == 0)
-                    || new_x >= self.size
-                    || new_y >= self.size
+                if (delta_x == 0 && delta_y == 0)
+                    || new_x as usize >= self.size
+                    || new_y as usize >= self.size
+                    || new_x < 0
+                    || new_y < 0
                 {
                     continue;
                 }
+
+                let new_x = new_x as usize;
+                let new_y = new_y as usize;
 
                 let neighbour = self.get(new_x, new_y);
 
@@ -187,15 +203,110 @@ impl Forest {
         }
         returned_vec
     }
+    fn get_lumber_jack_destinations(&self, x: usize, y: usize) -> Vec<(usize, usize)> {
+        let mut returned_vec = Vec::with_capacity(8);
+        for delta_x in [-1, 0, 1].iter().cloned() {
+            for delta_y in [-1, 0, 1].iter().cloned() {
+                let new_x = delta_x + x as i32;
+                let new_y = delta_y + y as i32;
+
+                if (delta_x == 0 && delta_y == 0)
+                    || new_x as usize >= self.size
+                    || new_y as usize >= self.size
+                    || new_x < 0
+                    || new_y < 0
+                {
+                    continue;
+                }
+
+                let new_x = new_x as usize;
+                let new_y = new_y as usize;
+
+                let neighbour = self.get(new_x, new_y);
+
+                match neighbour {
+                    Some(ForestFeature::Empty) | Some(ForestFeature::Tree(_)) => {
+                        returned_vec.push((new_x, new_y))
+                    }
+                    _ => (),
+                };
+            }
+        }
+        println!("{:?} {} {}", returned_vec, x, y);
+        returned_vec
+    }
     fn plant_sapling(&mut self, x: usize, y: usize) {
         let new_loc = self.layout[x * self.size + y];
 
         if let ForestFeature::Empty = new_loc {
+            println!("plant at {} {}", x, y);
             self.layout[x * self.size + y] =
                 ForestFeature::Tree(FloraVariant::Sapling(Seedling { current_age: 0 }));
         } else {
             panic!("planting in a non empty zone {} {} {:?}", x, y, new_loc);
         }
+    }
+    fn move_lumberjack(
+        &mut self,
+        new_x: usize,
+        new_y: usize,
+        wood: Woodcutter,
+        old_x: usize,
+        old_y: usize,
+    ) {
+        let new_loc = self.get(new_x, new_y).unwrap();
+
+        println!("pre move\n{}", self);
+        match new_loc {
+            ForestFeature::Empty => {
+                println!("move to empty");
+                self.layout[new_x * self.size + new_y] = ForestFeature::LumberJack(wood);
+            }
+            ForestFeature::Tree(l) => {
+                match l {
+                    FloraVariant::Sapling(a) => {
+                        println!("moved to sap");
+                        self.layout[new_x * self.size + new_y] =
+                            ForestFeature::LumberSeedling(wood, *a);
+                    }
+                    FloraVariant::Tree(_) => {
+                        println!("moved to tree");
+                        self.layout[new_x * self.size + new_y] =
+                            ForestFeature::LumberJack(Woodcutter {
+                                lumber_collected: wood.lumber_collected + 1,
+                                finished_moving: true,
+                            });
+                    }
+                    FloraVariant::Elder(_) => {
+                        println!("moved to elder");
+                        self.layout[new_x * self.size + new_y] =
+                            ForestFeature::LumberJack(Woodcutter {
+                                lumber_collected: wood.lumber_collected + 2,
+                                finished_moving: true,
+                            });
+                    }
+                };
+            }
+            _ => panic!("moved lumber to invalid position"),
+        }
+
+        let old_loc = self.get(old_x, old_y).unwrap();
+
+        println!("mid move\n{}", self);
+        match old_loc {
+            ForestFeature::LumberJack(_) => {
+                self.layout[old_x * self.size + old_y] = ForestFeature::Empty
+            }
+            ForestFeature::LumberSeedling(_, sap) => {
+                self.layout[old_x * self.size + old_y] =
+                    ForestFeature::Tree(FloraVariant::Sapling(*sap))
+            }
+            old_place => panic!(
+                "lumberjack disappered {:?} {} {},{},{}",
+                old_place, old_x, old_y, new_x, new_y
+            ),
+        }
+        println!("post move\n{}", self);
     }
 }
 
@@ -238,11 +349,19 @@ fn main() {
 
     let mut simulated_forest = Forest::new(size);
 
-    for year in (1..400) {
-        for month in (1..12) {
+    for year in 1..400 {
+        for month in 1..12 {
             simulated_forest.age_trees();
 
             process_spawning(&mut simulated_forest, &mut rng);
+
+            println!("{}", simulated_forest);
+
+            for move_phase in 0..5 {
+                if move_phase < 3 {
+                    process_lumberjacks(&mut simulated_forest, &mut rng);
+                }
+            }
 
             println!("{}", simulated_forest);
             println!("{:-<1$}", "", size * 2);
@@ -285,5 +404,21 @@ fn process_spawning(simulated_forest: &mut Forest, rng: &mut ThreadRng) {
             }
             _ => continue,
         }
+    }
+}
+fn process_lumberjacks(simulated_forest: &mut Forest, rng: &mut ThreadRng) {
+    let lumber_jack_locations = simulated_forest.get_lumberjacks();
+
+    for l in lumber_jack_locations {
+        let (old_x, old_y, wood) = l;
+        let new_destinations = simulated_forest.get_lumber_jack_destinations(old_x, old_y);
+        if new_destinations.is_empty() {
+            continue;
+        }
+        let (new_x, new_y) = new_destinations
+            .get(rng.gen_range(0, new_destinations.len()))
+            .unwrap();
+        println!("chose {} {}", new_x, new_y);
+        simulated_forest.move_lumberjack(*new_x, *new_y, wood, old_x, old_y);
     }
 }
